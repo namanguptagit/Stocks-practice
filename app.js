a99bd4484bcc7bd7efa71e90d046454b515851e3
Naman Gupta <naman312000@gmail.com>
2022-09-17 17:04:19 +0530

    .

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ function calculateProfitAndLoss(initial, quantity, current) {
         var loss = (initial - current) * quantity;
         var lossPercentage = (loss / initial) * 100;
         showOutput(`Hey the loss is ${loss} and the loss percent is ${lossPercentage}%`);
-    } else if (initial < current) {
+    } 
+    else if (current > initial) {
         var profit = (current - initial) * quantity;
         var profitPercentage = (profit / initial) * 100;
         showOutput(`Hey the profit is ${profit} and the profit percent is ${profitPercentage}%`);
-    } else {
+    } 
+    else {
         showOutput("No pain no gain and no gain no pain");
     }
 }
